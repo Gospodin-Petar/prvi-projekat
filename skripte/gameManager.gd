09a98d68db308e37a4b08ee9656b372959ecf397
Gospodin-Petar <wubbalubbadubdub11060@gmail.com>
2024-07-10 23:34:16 +0200

    dugme

--- skripte/gameManager.gd
+++ skripte/gameManager.gd
@@ -3,6 +3,7 @@ extends Node
 var mozeTP = 0
 var score = 0
 @onready var label_score = $labelScore
+@onready var animation_player = $"../platformeKratke/platformaKratka4/AnimationPlayer"
 
 
 func addPoint():
@@ -14,3 +15,7 @@ Fali ti jos: " + str(150 - score)
 		mozeTP = 1
 		label_score.text = "Prodji!
 score: " + str(score) + "/150"
+
+
+func _on_dugme_1_pressed():
+	animation_player.play("desno")
